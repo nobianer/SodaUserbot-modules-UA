042d561f68c8b23b16977d9875a4410fd7ea8317
nobianer <138612936+nobianer@users.noreply.github.com>
2024-03-05 19:04:33 +0200

    Update AFK.py

--- modules/AFK.py
+++ modules/AFK.py
@@ -26,8 +26,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# meta developer: @nobianermodules
-
 import datetime
 import logging
 import time
