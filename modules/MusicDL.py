e21bf2b4a9b21aac703093f23d827e1c79695068
nobianer <138612936+nobianer@users.noreply.github.com>
2024-08-11 00:55:55 +0300

    Update MusicDL.py

--- modules/MusicDL.py
+++ modules/MusicDL.py
@@ -8,7 +8,7 @@
 
 # meta pic: https://static.dan.tatar/musicdl_icon.png
 # meta banner: https://mods.hikariatama.ru/badges/musicdl.jpg
-# meta developer: @hikarimods
+# meta developer: @nobianermodules
 # scope: hikka_only
 # scope: hikka_min 1.3.0
 
