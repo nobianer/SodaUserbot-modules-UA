080c714b10dcdf2ae8bdc0df678b653ddb4214de
nobianer <138612936+nobianer@users.noreply.github.com>
2024-08-11 00:56:53 +0300

    Update ServerInfo.py

--- modules/ServerInfo.py
+++ modules/ServerInfo.py
@@ -4,7 +4,7 @@
 #  > ^ <   ⚠️ Owner of heta.hikariatama.ru doesn't take any responsibilities or intellectual property rights regarding this script
 # ---------------------------------------------------------------------------------
 # Name: serverinfo
-# Author: hikariatama
+# meta developer: @nobianermodules
 # Commands:
 # .serverinfo
 # ---------------------------------------------------------------------------------
